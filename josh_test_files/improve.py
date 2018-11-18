1927e2db6a04af3b8266f81b4f4ac49b3b602eea
Josuha Engels <joshuaengels@yahoo.com>
2018-11-18 12:30:11 -0600

    Made some nice functions to make testing with hexes easier

--- josh_test_files/improve.py
+++ josh_test_files/improve.py
@@ -28,39 +28,78 @@ def random_greedy(node_list):
 
 
 def update(graph, nodes):
-    temp = copy.copy(graph)
+    old = copy.copy(graph)
 
     simplify_area(nodes)
     random_greedy(nodes)
 
-    if temp.calculate_score() >= graph.calculate_score():
-        graph = temp
+    if old.calculate_score() >= graph.calculate_score():
+        graph = old
 
     return graph
 
 
-n = 8
-trials = 200
-improvements = 200
-old_score = 0
+def hex_improve(graph, runs_through_the_graph, max_hex_size, best_score_so_far):
+    """
+    Loops through every element in the graph multiple times and tries to improve hexes from size 1 to max_hex_size
+    at every one of those elements
+    :param graph: The graph to attempt to improve
+    :param runs_through_the_graph: The number of times to loop through every element in the graph
+    :param max_hex_size: The max hex size to test
+    :param best_score_so_far: The best score attained for this n so far. The function will print
+    out scores found greater than this score, as well as grids.
+    :return: The graph, improved if possible or otherwise the same
+    """
+    best_score_so_far = max(best_score_so_far, graph.calculate_score())
 
-while True:
-    original = test3.fill_ones(n, trials)
-    node_list = original.get_node_list()
+    for temp in range(runs_through_the_graph):
 
-    for temp in range(improvements):
-        # print(temp)
+        for i in range(len(graph.get_node_list())):
+            for j in range(1, max_hex_size + 1):
 
-        for i in range(len(node_list)):
-            for j in range(5):
-                node_list = original.get_node_list()
-                original = update(original, hex_grid_graph.hex_grid.get_hex(node_list[i], j))
-                if original.calculate_score() > old_score:
-                    old_score = original.calculate_score()
-                    print(old_score)
+                node_list = graph.get_node_list()
+                graph = update(graph, hex_grid_graph.hex_grid.get_hex(node_list[i], j))
+
+                if graph.calculate_score() > best_score_so_far:
+                    best_score_so_far = original.calculate_score()
+                    print(best_score_so_far)
                     original.print_submission()
                     print()
 
+    return graph
+
+
+original = hex_grid_graph.hex_grid.create_from_2d_list([
+(4, 3, 5, 4, 3, 5, 4, 3, 4, 2),
+(2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 3),
+(3, 4, 7, 6, 4, 3, 4, 3, 4, 5, 6, 4),
+(1, 6, 5, 3, 5, 7, 6, 5, 6, 7, 3, 2, 1),
+(1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 7, 6, 4),
+(4, 5, 6, 7, 5, 4, 3, 4, 3, 4, 6, 4, 5, 3, 2),
+(3, 2, 3, 4, 3, 7, 6, 5, 7, 6, 5, 3, 2, 1, 4, 1),
+(5, 1, 5, 1, 2, 1, 2, 1, 2, 1, 2, 1, 4, 6, 7, 5, 3),
+(4, 2, 7, 4, 6, 5, 7, 6, 5, 3, 5, 6, 7, 5, 3, 2, 1, 2),
+(3, 1, 6, 3, 2, 3, 4, 3, 4, 7, 6, 4, 3, 2, 1, 7, 6, 5, 1),
+(2, 5, 4, 1, 2, 1, 2, 1, 2, 1, 2, 1, 4, 6, 5, 4, 3, 4),
+(3, 7, 2, 4, 3, 4, 3, 4, 3, 4, 7, 5, 7, 3, 2, 1, 2),
+(1, 6, 5, 6, 7, 5, 6, 7, 5, 6, 3, 2, 1, 4, 5, 3),
+(4, 3, 1, 2, 1, 2, 1, 2, 1, 2, 1, 3, 7, 6, 1),
+(2, 6, 5, 7, 6, 5, 7, 6, 5, 6, 4, 5, 2, 3),
+(1, 4, 3, 4, 3, 4, 3, 4, 3, 7, 2, 1, 4),
+(5, 2, 1, 2, 1, 2, 1, 2, 1, 5, 3, 5),
+(3, 5, 4, 5, 6, 4, 5, 3, 4, 6, 2),
+(1, 2, 3, 1, 3, 2, 1, 5, 2, 1)
+])
+
+
+# n = 7
+trials = 400
+improvements = 2000
+old_score = 0
+
+while True:
+    hex_improve(original, improvements, 6, 0)
+    break
 
 # node_map = hex_grid_graph.hex_grid(7)
 # node_map.print_submission()
